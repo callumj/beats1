f947ab4c43ff7d4e96812f1ef75e4ccbea776ecc
Callum Jones <callum@callumj.com>
2015-08-23 22:40:08 -0700

    Move searching first

--- beats1/mb_searcher.rb
+++ beats1/mb_searcher.rb
@@ -9,6 +9,16 @@ module Beats1
     end
 
     def self.search_mb(artist)
+      if defined?(::Artist)
+        a = Artist.find_by_name(artist)
+        if a
+          return SearchResult.new.tap do |s|
+            s.identifier = "internal:#{a.id}"
+            s.twitter = a.twitter
+          end
+        end
+      end
+
       result = begin
         Timeout::timeout(5) do
           Beats1::MBClient.find_artist artist
@@ -17,31 +27,12 @@ module Beats1
         nil
       end
 
-      ret = nil
       if result
-        ret = SearchResult.new.tap do |s|
+        return SearchResult.new.tap do |s|
           s.identifier = result[:artist].id
           s.twitter = result[:twitter]
         end
       end
-
-      return ret if ret && ret.twitter
-
-      # we are fine with no model being available (db not loaded)
-      return nil unless defined?(::Artist)
-
-      # fall back to this gap fill model
-      base = Artist.where("name = ?", artist)
-      if ret
-        base = base.or("mb_id = ?", ret.identifier)
-      end
-      if a = base.first
-        return SearchResult.new.tap do |s|
-          s.identifier = "internal:#{a.id}"
-          s.twitter = a.twitter
-        end
-      end
-
       nil
     end
 
